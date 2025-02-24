67ab194079e1ee7d24486e744b20a50cedded379
letstryi <belkabousyassir@gmail.com>
2025-02-24 22:47:21 +0000

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get ('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+    
+//require __DIR__. '/auth.php';
+    
+Route:: prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+    // Admin Login Route
+    Route::match( ['get','post'],'login', 'AdminController@login');
+    Route::group( ['middleware'=>['admin' ]], function(){
+        // Admin Dashboard Route
+        Route::get('dashboard','AdminController@dashbord');
+        // Admin logout
+        Route::get('logout','AdminController@logout');
+    });
+});
